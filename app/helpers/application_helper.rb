61413275cec5b6b0d23c5ec2076bf435f0b996dc
Vinnie Padmanabhan <vinnie329@gmail.com>
2014-02-19 17:28:16 +0000

    New Profile Page Layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,8 +11,14 @@ module ApplicationHelper
 
   def show_user_bg
     "background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.25)), url(#{@user.wallpaper});
+     background-image: -moz-linear-gradient(to bottom, rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.25)), url(#{@user.wallpaper});
+     background-image: -o-linear-gradient(to bottom, rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.25)), url(#{@user.wallpaper});
+     background-image: -ms-linear-gradient(to bottom, rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.25)), url(#{@user.wallpaper});
+     background-image: -webkit-gradient(linear, left top, left bottom, from(rgba(0, 0, 0, 0.25)), to(rgba(0, 0, 0, 0.25))), url(#{@user.wallpaper});
+     background-image: -webkit-linear-gradient(top, rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.25)), url(#{@user.wallpaper});
      background-size: cover;
      background-position: center;
-     height: 450px;"
+     height: 100%;
+     width: 40%;"
   end
 end
