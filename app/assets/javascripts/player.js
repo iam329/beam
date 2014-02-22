2d78bd9bf749e7222b220d3caf5be012edd444f3
Vinnie Padmanabhan <vinnie329@gmail.com>
2014-02-22 23:17:53 +0000

    Gem player JS

--- app/assets/javascripts/player.js
+++ app/assets/javascripts/player.js
@@ -1,12 +1,17 @@
 $(document).ready(function() {
+
 	$("button").click(function() {
 	
 	var vid = $(this).attr("vid");
 	var title = $(this).attr("title");
 	var artist = $(this).attr("artist"); 
+	var episode = $(this).attr("episode");
+	var created = $(this).attr("created");
 	$(".youtube-player").attr("src", vid);
-	$(".track-info").find("h5").text(title);
-
+	$(".track-info").find("#episode-number").text("Episode "+episode);
+	$(".track-info").find("#track-title").find("h4").text(title);
+	$(".track-info").find("#artist-name").text(artist);
+	$(".track-info").find("#timestamp").text(created);
 	});
 });
 
