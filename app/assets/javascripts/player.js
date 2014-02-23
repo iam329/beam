14a2c13829da151daa0791b56f006307c475e74d
Vinnie Padmanabhan <vinnie329@gmail.com>
2014-02-23 20:25:36 +0000

    Prelim version of Feed

--- app/assets/javascripts/player.js
+++ app/assets/javascripts/player.js
@@ -13,6 +13,21 @@ $(document).ready(function() {
 	$(".track-info").find("#artist-name").text(artist);
 	$(".track-info").find("#timestamp").text(created+" ago");
 	});
+
+	$(".home-feature").find("button").click(function() {
+	
+	var vid = $(this).attr("vid");
+	var title = $(this).attr("title");
+	var artist = $(this).attr("artist"); 
+	var episode = $(this).attr("episode");
+	var created = $(this).attr("created");
+	$(".youtube-player").attr("src", vid);
+	$(".track-info").find("#episode-number").text("Episode "+episode);
+	$(".track-info").find("#track-title").find("h4").text(title);
+	$(".track-info").find("#artist-name").text(artist);
+	$(".track-info").find("#timestamp").text(created+" ago");
+	});
+
 });
 
 
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,8 @@ class StaticPagesController < ApplicationController
   def home
   	if signed_in?
   		@feed_items = current_user.feed
+  		@latestpost = @feed_items.first
+  		@count = @feed_items.count
   	end
   end
 
