2f7a4815da433d72929426246862141378053c27
Vinnie Padmanabhan <vinnie329@gmail.com>
2014-02-24 13:56:33 +0000

    Feed JS and styling

--- app/assets/javascripts/player.js
+++ app/assets/javascripts/player.js
@@ -7,6 +7,7 @@ $(document).ready(function() {
 	var artist = $(this).attr("artist"); 
 	var episode = $(this).attr("episode");
 	var created = $(this).attr("created");
+	
 	$(".youtube-player").attr("src", vid);
 	$(".track-info").find("#episode-number").text("Episode "+episode);
 	$(".track-info").find("#track-title").find("h4").text(title);
@@ -19,13 +20,17 @@ $(document).ready(function() {
 	var vid = $(this).attr("vid");
 	var title = $(this).attr("title");
 	var artist = $(this).attr("artist"); 
-	var episode = $(this).attr("episode");
 	var created = $(this).attr("created");
+	var photo = $(this).attr("photo");
+	var username = $(this).attr("user");
+	
 	$(".youtube-player").attr("src", vid);
-	$(".track-info").find("#episode-number").text("Episode "+episode);
 	$(".track-info").find("#track-title").find("h4").text(title);
 	$(".track-info").find("#artist-name").text(artist);
-	$(".track-info").find("#timestamp").text(created+" ago");
+	$(".track-info").find("#timestamp").find("#username").text(username);
+	$(".track-info").find("#timestamp").find("#time").text("Posted "+created+ " ago by ");
+	$(".track-info").find("#profile-photo").find("img").attr("src", photo);
+
 	});
 
 });
