e698ab141c3e5673e9bc6c1f76fa218ab583eb6a
Caleb Chiu <zero.caleb@gmail.com>
2014-02-24 22:41:42 +0000

    Revert 16873ff..7654656
    This rolls back to commit 16873fffbbe25aaed7c3ae869e9fae3318372273.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,16 +1,9 @@
 class StaticPagesController < ApplicationController
   def home
-<<<<<<< HEAD
   	if signed_in?
   		@feed_items = current_user.feed
   		@latestpost = @feed_items.first
   		@count = @feed_items.count
-=======
- 	if signed_in?
-  		@feed_items_user = current_user.feed
-  		@latestpost_user = @feed_items_user.first
-  		@count_user = @feed_items_user.count	
->>>>>>> parent of 16873ff... Updated home page for signed in and visitor mode
   	end
   end
 
