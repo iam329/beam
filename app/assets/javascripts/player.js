827d355217781cabba5224547509cd72cc9e419b
Caleb Chiu <zero.caleb@gmail.com>
2014-02-25 00:53:46 +0000

    Revert To Stable Version

--- app/assets/javascripts/player.js
+++ app/assets/javascripts/player.js
@@ -1,39 +1,37 @@
 $(document).ready(function() {
 
-	$(".feature").find("button").click(function() {
-	
-	var vid = $(this).attr("vid");
-	var title = $(this).attr("title");
-	var artist = $(this).attr("artist"); 
-	var episode = $(this).attr("episode");
-	var created = $(this).attr("created");
-	
-	$(".youtube-player").attr("src", vid);
-	$(".track-info").find("#episode-number").text("Episode "+episode);
-	$(".track-info").find("#track-title").find("h4").text(title);
-	$(".track-info").find("#artist-name").text(artist);
-	$(".track-info").find("#timestamp").text(created+" ago");
-	});
+        $(".feature").find("button").click(function() {
 
-	$(".home-feature").find("button").click(function() {
-	
-	var vid = $(this).attr("vid");
-	var title = $(this).attr("title");
-	var artist = $(this).attr("artist"); 
-	var created = $(this).attr("created");
-	var photo = $(this).attr("photo");
-	var username = $(this).attr("user");
-	
-	$(".youtube-player").attr("src", vid);
-	$(".track-info").find("#track-title").find("h4").text(title);
-	$(".track-info").find("#artist-name").text(artist);
-	$(".track-info").find("#timestamp").find("#username").text(username);
-	$(".track-info").find("#timestamp").find("#time").text("Posted "+created+ " ago by ");
-	$(".track-info").find("#profile-photo").find("img").attr("src", photo);
+        var vid = $(this).attr("vid");
+        var title = $(this).attr("title");
+        var artist = $(this).attr("artist");
+        var episode = $(this).attr("episode");
+        var created = $(this).attr("created");
 
-	});
+        $(".youtube-player").attr("src", vid);
+        $(".track-info").find("#episode-number").text("Episode "+episode);
+        $(".track-info").find("#track-title").find("h4").text(title);
+        $(".track-info").find("#artist-name").text(artist);
+        $(".track-info").find("#timestamp").text(created+" ago");
+        });
+
+        $(".home-feature").find("button").click(function() {
+
+        var vid = $(this).attr("vid");
+        var title = $(this).attr("title");
+        var artist = $(this).attr("artist");
+        var created = $(this).attr("created");
+        var photo = $(this).attr("photo");
+        var username = $(this).attr("user");
+
+        $(".youtube-player").attr("src", vid);
+        $(".track-info").find("#track-title").find("h4").text(title);
+        $(".track-info").find("#artist-name").text(artist);
+        $(".track-info").find("#timestamp").find("#username").find("a").text(username);
+        $(".track-info").find("#timestamp").find("#time").text("Posted "+created+ " ago by ");
+        $(".track-info").find("#profile-photo").find("img").attr("src", photo);
+
+        });
 
 });
-
-
 	
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,83 +1,81 @@
 class UsersController < ApplicationController
-	before_action :signed_in_user, only:[:edit, :update]
-	before_action :correct_user, only:[:edit, :update]
+    before_action :signed_in_user, only:[:edit, :update]
+    before_action :correct_user, only:[:edit, :update]
 
-	def show
- 		@user = User.find_by_name(params[:name])
- 		
- 		if @user
- 			@posts = @user.posts
- 			@latestpost = @posts.first
- 			@count = @posts.count
- 		else
- 			redirect_to root_path
- 		end
- 	end
+    def show
+       @user = User.find_by_name(params[:name])
 
-	def new
-	  	@user = User.new		
-	end
+       if @user
+          @posts = @user.posts
+          @latestpost = @posts.first
+          @count = @posts.count
+       else
+          redirect_to root_path
+       end
+    end
 
-	def create
-		@user=User.new(user_params)
+    def new
+       @user = User.new
+    end
 
-		if @user.save
-			sign_in @user
-			flash[:success] = "Welcome to TapeDeckHero! What's your track of the day?"
-			redirect_to @user
-		else
-		 	render 'new'
-		end	
-	end
+    def create
+       @user=User.new(user_params)
 
-	def edit
-		@user=User.find(params[:id])
-	end
+       if @user.save
+           sign_in @user
+           redirect_to profile_path(@user.name)
+       else
+           render 'new'
+       end
+    end
 
-	def update
-    	@user = User.find(params[:id])
-   	 	
-   	 	if @user.update_attributes(user_params)
-      		flash[:success] = "Profile Updated"
-      		redirect_to profile_path(@user.name)
-    	else
-      		render 'edit'
-    	end
-  	end
+    def edit
+       @user=User.find(params[:id])
+    end
 
-	def destroy
-	end
+    def update
+       @user = User.find(params[:id])
 
-	def following
-		@user = User.find(params[:id])
-    	@users = @user.followed_users
-    	@count = @user.posts.count
-    	render 'show_following'
-	end
+       if @user.update_attributes(user_params)
+           redirect_to profile_path(@user.name)
+       else
+           render 'edit'
+       end
+    end
 
-	def followers
-		@user = User.find(params[:id])
-    	@users = @user.followers
-    	@count = @user.posts.count
-    	render 'show_follower'
-	end
+    def destroy
+    end
 
-	private
+    def following
+       @user = User.find(params[:id])
+       @users = @user.followed_users
+       @count = @user.posts.count
+       render 'show_following'
+    end
 
-	def user_params
-	  	params.require(:user).permit(:name, 
-	  								 :email, 
-	  								 :bio, 
-	  								 :password,
-                                     :password_confirmation,
-                                     :photo,
-                                     :wallpaper)	
-	end
+    def followers
+       @user = User.find(params[:id])
+       @users = @user.followers
+       @count = @user.posts.count
+       render 'show_follower'
+    end
 
-	def correct_user
-    	@user = User.find(params[:id])
-    	unless current_user == @user
-      	redirect_to root_path
-    	end
-  	end	
+    private
+
+       def user_params
+           params.require(:user).permit(:name,
+                                        :email,
+                                        :bio,
+                                        :password,
+                                        :password_confirmation,
+                                        :photo,
+                                        :wallpaper)
+       end
+
+       def correct_user
+           @user = User.find(params[:id])
+           unless current_user == @user
+           redirect_to root_path
+       end
+    end
 end
