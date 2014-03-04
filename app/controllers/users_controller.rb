89ee38a9a133055a8333d3ca64bb9e9f4e66bb15
Vinnie Padmanabhan <vinnie329@gmail.com>
2014-03-04 22:48:45 +0000

    Changed background of html

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,7 +23,6 @@ class UsersController < ApplicationController
 
        if @user.save
            sign_in @user
-           UserMailer.welcome(@user)
            redirect_to profile_path(@user.name)
        else
            render 'new'
